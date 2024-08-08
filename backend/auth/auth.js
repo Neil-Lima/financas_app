1932dceacd4ba0998513fc50c1e2ce2006da2015
Neil-Lima <110132286+Neil-Lima@users.noreply.github.com>
2024-08-08 09:41:38 -0300

    d

--- backend/auth/auth.js
+++ backend/auth/auth.js
@@ -1,6 +1,5 @@
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-
+const bcrypt = require('bcryptjs')
 const generateToken = (user) => {
   return jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1d' });
 };
